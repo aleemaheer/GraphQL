ce992597e305f8485a8b741a8cd4a012a6e19445
aleem aheer <aleemaheer5769644@gmail.com>
2022-11-16 21:45:33 +0500

    Added some more data

--- schema/schema.js
+++ schema/schema.js
@@ -12,11 +12,26 @@ const BookType = new GraphQLObjectType({
 	}),
 });
 
+const pcType = new GraphQLObjectType({
+	name: "pc",
+	fields: () => ({
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		company: { type: GraphQLString },
+	}),
+});
+
 // some data
 const data = [
 	{ id: "1", name: "The Endgame", genre: "Awesome" },
 	{ id: "2", name: "The Node.js", genre: "Backend" },
 	{ id: "3", name: "The Long Earth", genre: "Exploration" },
+];
+
+const pcData = [
+	{ id: "1", name: "First Mark-1", company: "Intel" },
+	{ id: "2", name: "First Mark-2", company: "DELL" },
+	{ id: "3", name: "First Mark-3", company: "HP" },
 ];
 
 const RootQuery = new GraphQLObjectType({
@@ -29,6 +44,14 @@ const RootQuery = new GraphQLObjectType({
 				// code to get data from db or other source
 				//console.log(_.find(data, { id: args.id }));
 				return _.find(data, { id: args.id });
+			},
+		},
+		pc: {
+			type: pcType,
+			args: { id: { type: GraphQLString } },
+			resolve(parent, args) {
+				// Return the pc
+				return _.find(pcData, { id: args.id });
 			},
 		},
 	}),
