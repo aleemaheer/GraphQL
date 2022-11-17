a721f5583e110d8963b47fa4b34a295ce63e1a77
aleem aheer <aleemaheer5769644@gmail.com>
2022-11-17 14:05:36 +0500

    Added books, authors

---
+++ data.js
@@ -0,0 +1,20 @@
+// some data
+const data = [
+	{ id: "1", name: "The Endgame", genre: "Awesome" },
+	{ id: "2", name: "The Node.js", genre: "Backend" },
+	{ id: "3", name: "The Long Earth", genre: "Exploration" },
+];
+
+const pcData = [
+	{ id: "1", name: "First Mark-1", company: "Intel" },
+	{ id: "2", name: "First Mark-2", company: "DELL" },
+	{ id: "3", name: "First Mark-3", company: "HP" },
+];
+
+const author = [
+	{ name: "Patrick", age: 44, id: "1" },
+	{ name: "John", age: 24, id: "2" },
+	{ name: "Tom", age: 34, id: "3" },
+];
+
+module.exports = (data, pcData, author);
--- schema/schema.js
+++ schema/schema.js
@@ -1,57 +1,111 @@
 const graphql = require("graphql");
 const _ = require("lodash");
+const authors = require("../data");
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+	GraphQLObjectType,
+	GraphQLString,
+	GraphQLSchema,
+	GraphQLID,
+	GraphQLInt,
+	GraphQLList,
+} = graphql;
 
-const BookType = new GraphQLObjectType({
-	name: "Book",
-	fields: () => ({
-		id: { type: GraphQLString },
-		name: { type: GraphQLString },
-		genre: { type: GraphQLString },
-	}),
-});
-
-const pcType = new GraphQLObjectType({
-	name: "pc",
-	fields: () => ({
-		id: { type: GraphQLString },
-		name: { type: GraphQLString },
-		company: { type: GraphQLString },
-	}),
-});
-
-// some data
-const data = [
-	{ id: "1", name: "The Endgame", genre: "Awesome" },
-	{ id: "2", name: "The Node.js", genre: "Backend" },
-	{ id: "3", name: "The Long Earth", genre: "Exploration" },
+// Books
+const books = [
+	{ id: "4", name: "Behind The Scenes", genre: "Documentary", authorId: "1" },
+	{ id: "1", name: "The Endgame", genre: "Awesome", authorId: "1" },
+	{ id: "2", name: "The Node.js", genre: "Backend", authorId: "2" },
+	{ id: "3", name: "The Long Earth", genre: "Exploration", authorId: "3" },
 ];
 
+// PCs
 const pcData = [
 	{ id: "1", name: "First Mark-1", company: "Intel" },
 	{ id: "2", name: "First Mark-2", company: "DELL" },
 	{ id: "3", name: "First Mark-3", company: "HP" },
 ];
 
+// Book Type
+const BookType = new GraphQLObjectType({
+	name: "Book",
+	fields: () => ({
+		id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		genre: { type: GraphQLString },
+		author: {
+			type: AuthorType,
+			resolve(parent, args) {
+				return _.find(authors, { id: parent.authorId });
+			},
+		},
+	}),
+});
+
+// PC Type
+const PCType = new GraphQLObjectType({
+	name: "pc",
+	fields: () => ({
+		id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		company: { type: GraphQLString },
+	}),
+});
+
+// Author Type
+const AuthorType = new GraphQLObjectType({
+	name: "author",
+	fields: () => ({
+		id: { type: GraphQLID },
+		name: { type: GraphQLString },
+		age: { type: GraphQLInt },
+		books: {
+			type: new GraphQLList(BookType),
+			resolve(parent, args) {
+				console.log({ parent });
+				return _.filter(books, { authorId: parent.id });
+			},
+		},
+	}),
+});
+
 const RootQuery = new GraphQLObjectType({
 	name: "RootQueryType",
 	fields: () => ({
 		book: {
 			type: BookType,
-			args: { id: { type: GraphQLString } },
+			args: { id: { type: GraphQLID } },
 			resolve(parent, args) {
 				// code to get data from db or other source
 				//console.log(_.find(data, { id: args.id }));
-				return _.find(data, { id: args.id });
+				return _.find(books, { id: args.id });
 			},
 		},
 		pc: {
-			type: pcType,
-			args: { id: { type: GraphQLString } },
+			type: PCType,
+			args: { id: { type: GraphQLID } },
 			resolve(parent, args) {
 				// Return the pc
 				return _.find(pcData, { id: args.id });
+			},
+		},
+		author: {
+			type: AuthorType,
+			args: { id: { type: GraphQLID } },
+			resolve(parent, args) {
+				return _.find(authors, { id: args.id });
+			},
+		},
+		books: {
+			type: new GraphQLList(BookType),
+			resolve(parent, args) {
+				return books;
+			},
+		},
+		authors: {
+			type: new GraphQLList(AuthorType),
+			resolve(parent, args) {
+				return authors;
 			},
 		},
 	}),
